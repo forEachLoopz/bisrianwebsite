305ae04018b171cb4fc29f0172c69361a5d22bf4
forEachLoopz <juliolgsage@gmail.com>
2024-04-25 16:02:16 +0200

    firstcommit

---
+++ navigation.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(){
+    const buyUndergroundDogs = document.getElementById('buyUndergroundDogs');
+
+    buyUndergroundDogs.addEventListener("click", function(){
+        window.open("https://bisrian.bandcamp.com/album/underground-dogs-3");
+    })
+})
